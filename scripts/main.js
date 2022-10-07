1e486c7ca95899afbf98e44ff9dff845bf1adedc
Cristi Neames <crisneames@gmail.com>
2022-10-06 20:54:33 -0500

    catDog logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-import { fetchRandomDog } from "./dataAccess.js";
+import { catsDogs } from './catsDogs.js';
+import { fetchRandomDog, fetchRandomCat } from './dataAccess.js';
+
+const container = document.querySelector('#voting');
+
+const render = async () => {
+  await fetchRandomDog();
+  await fetchRandomCat();
+
+  container.innerHTML = catsDogs();
+};
+
+render();
