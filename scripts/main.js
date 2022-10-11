02663890ba4ab11915aef173bb9cf34bb4189201
alvinwendt <alvinwendt@gmail.com>
2022-10-10 19:21:39 -0500

    Added selection css selection functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 import { fetchRandomDog } from "./dataAccess.js";
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.id.startsWith("dog")) {
+    itemClicked.classList.add("selected");
+    let cat = document.getElementById("cat--APIIDGOESHERE");
+    cat.classList.remove("selected");
+  } else if (itemClicked.id.startsWith("cat")) {
+    itemClicked.classList.add("selected");
+    let dog = document.getElementById("dog--APIIDGOESHERE");
+    dog.classList.remove("selected");
+  }
+});
